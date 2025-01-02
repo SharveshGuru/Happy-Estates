52626751ae3a4a4f9f6a763aa009d59ad483b450
SharveshGuru <sharveshguru2004@gmail.com>
2025-01-02 20:26:13 +0530

    wip-property listings page

--- reactapp/src/components/PropertyListings/PropertyListings.jsx
+++ reactapp/src/components/PropertyListings/PropertyListings.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import styles from "./PropertyListings.module.css";
 const PropertyListings = () =>{
     return(
         <div>
-            <h1>Property Listings</h1>
+            <h1 className={styles.heading}>Property Listings</h1>
+            <div className={styles.container}>
+                <h1>hi</h1>
+                <div className={styles.listing}>
+
+                </div>
+            </div>
         </div>
     )
 }
