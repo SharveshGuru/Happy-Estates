1369e12a53d5f0a144e8bd1495b8d62beb363fd3
SharveshGuru <sharveshguru2004@gmail.com>
2025-01-25 20:07:36 +0530

    handled edge case when user isn't linked with any property

--- reactapp/src/components/Payments/Payments.jsx
+++ reactapp/src/components/Payments/Payments.jsx
@@ -9,6 +9,7 @@ const Payments = () =>{
     const user=JSON.parse(localStorage.getItem("user"));
     const [open,setOpen]=useState(false);
     const [payments,setPayments]=useState([]);
+    const [valid,setValid]=useState(false);
     function handlePay(){
         setOpen(!open);
     }
@@ -19,21 +20,21 @@ const Payments = () =>{
             axiosInstance.get(`/tenantpayments/${user.sub}`)
             .then((res)=>{
                 setPayments(res.data);
-                console.log(res.data);
+                setValid(true);
             })
             .catch((err)=>{
-                window.alert("Error getting payments!");
+                window.alert("Error getting payments! Maybe you aren't linked to any property yet!");
             });
         }
 
         if(user.role==="ROLE_Owner"){
             axiosInstance.get(`/ownerpayments/${user.sub}`)
             .then((res)=>{
+                setValid(true);
                 setPayments(res.data);
-                console.log(res.data);
             })
             .catch((err)=>{
-                window.alert("Error getting payments!");
+                window.alert("Error getting payments! Maybe you aren't linked to any property yet!");
             });
         }
     },[user.sub,user.role,open]);
@@ -43,38 +44,40 @@ const Payments = () =>{
             <h1 className={styles.heading}>Payments</h1>
             <div className={styles.container}>
                 <div className={styles.listing}>
-                    <div className={styles.listingHeader}>
-                        <h2>Payment History</h2>
-                        <button onClick={handlePay} className={styles.makePaymentButton}>Make Payment</button>
-                    </div><br />
-                    {payments.length>0 ? (<table className={styles.table}>
-                        <thead>
-                            <tr className={styles.headerRow}>
-                                <th className={styles.th}>Property</th>
-                                <th className={styles.th}>Made By</th>
-                                <th className={styles.th}>Description</th>
-                                <th className={styles.th}>Amount</th>
-                                <th className={styles.th}>Date</th>
-                                <th className={styles.th}>Remarks</th>
-                                <th className={styles.th}>Proof</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {payments.map((payment,index)=>(
-                                <tr key={index} className={styles.row}>
-                                    <td className={styles.td}>{payment.lease.property.name}</td>
-                                    <td className={styles.td}>{payment.paymentMadeBy.name}</td>
-                                    <td className={styles.td}>{payment.description}</td>
-                                    <td className={styles.td}>₹{payment.amount}</td>
-                                    <td className={styles.td}>{format(new Date(payment.paymentDate),"dd MMMM yyyy")}</td>
-                                    <td className={styles.td}>{payment.remarks}</td>
-                                    <td className={styles.td + " "+ styles.buttonRow}>
-                                        <button className={styles.actionButton}>View</button>    
-                                        <button className={styles.actionButton}>Download</button>
-                                    </td>
-                            </tr>))}
-                        </tbody>
-                    </table>):<h3>No payments have been made yet!</h3>}
+                    {valid ? (<>
+                        <div className={styles.listingHeader}>
+                            <h2>Payment History</h2>
+                            <button onClick={handlePay} className={styles.makePaymentButton}>Make Payment</button>
+                        </div><br />
+                        {payments.length>0 ? (<table className={styles.table}>
+                            <thead>
+                                <tr className={styles.headerRow}>
+                                    <th className={styles.th}>Property</th>
+                                    <th className={styles.th}>Made By</th>
+                                    <th className={styles.th}>Description</th>
+                                    <th className={styles.th}>Amount</th>
+                                    <th className={styles.th}>Date</th>
+                                    <th className={styles.th}>Remarks</th>
+                                    <th className={styles.th}>Proof</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {payments.map((payment,index)=>(
+                                    <tr key={index} className={styles.row}>
+                                        <td className={styles.td}>{payment.lease.property.name}</td>
+                                        <td className={styles.td}>{payment.paymentMadeBy.name}</td>
+                                        <td className={styles.td}>{payment.description}</td>
+                                        <td className={styles.td}>₹{payment.amount}</td>
+                                        <td className={styles.td}>{format(new Date(payment.paymentDate),"dd MMMM yyyy")}</td>
+                                        <td className={styles.td}>{payment.remarks}</td>
+                                        <td className={styles.td + " "+ styles.buttonRow}>
+                                            <button className={styles.actionButton}>View</button>    
+                                            <button className={styles.actionButton}>Download</button>
+                                        </td>
+                                </tr>))}
+                            </tbody>
+                        </table>):<h3>No payments have been made yet!</h3>}</>
+                    ):<h3>You have no payments to show!</h3>}
                 </div>
             </div>
 
