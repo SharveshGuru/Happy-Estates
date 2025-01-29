2378809083bd10805145fb5a3c933b7d3cb8b3e3
SharveshGuru <sharveshguru2004@gmail.com>
2025-01-29 09:26:23 +0530

    removed unused imports

--- artf/src/main/java/com/prjgrp/artf/controller/PaymentController.java
+++ artf/src/main/java/com/prjgrp/artf/controller/PaymentController.java
@@ -13,7 +13,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prjgrp.artf.model.Payment;
 import com.prjgrp.artf.service.PaymentService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
