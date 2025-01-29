6fed7d3efe2642b4139782a2fbf8f20ef5e9d211
SharveshGuru <sharveshguru2004@gmail.com>
2025-01-29 09:13:04 +0530

    update JacksonConfig

--- artf/src/main/java/com/prjgrp/artf/config/JacksonConfig.java
+++ artf/src/main/java/com/prjgrp/artf/config/JacksonConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
@@ -11,6 +12,7 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper ObjectMapper(){
         ObjectMapper obm=new ObjectMapper();
+        obm.registerModule(new Jdk8Module());
         obm.registerModule(new JavaTimeModule());
         return obm;
     }
