0521e820a08d4b32abf946cbb79867345b476271
Sharvesh <sharveshguru2004@gmail.com>
2025-02-16 17:04:18 +0530

    update to integrate postgresql

--- artf/src/main/java/com/prjgrp/artf/ArtfApplication.java
+++ artf/src/main/java/com/prjgrp/artf/ArtfApplication.java
@@ -4,18 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 @EnableScheduling
 public class ArtfApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv=Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PWD", dotenv.get("DB_PWD"));
-		System.setProperty("FRONTEND_URL", dotenv.get("FRONTEND_URL"));
+		System.setProperty("DB_URL", System.getenv("DB_URL"));
+		System.setProperty("DB_USER", System.getenv("DB_USER"));
+		System.setProperty("DB_PWD", System.getenv("DB_PWD"));
+		System.setProperty("FRONTEND_URL", System.getenv("FRONTEND_URL"));
+		System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
 		
 		SpringApplication.run(ArtfApplication.class, args);
 		
